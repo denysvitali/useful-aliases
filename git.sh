9f9e6b5524688ec6f36c14ca3bd4c9af774127d7
Denys Vitali <denys.vitali@coelis.ch>
2017-02-15 17:07:18 +0100

    Remove usage

--- git.sh
+++ git.sh
@@ -4,7 +4,5 @@ alias gpo  = 'git push origin $(git rev-parse --abbrev-ref HEAD)'
 alias gcom = 'git commit -m'
 alias gadd = 'git add'
 function gagh(){
- # Usage: gagh username/repository
- # Adds the GitHub repository as origin
  git remote add origin git@github.com:$1.git
 }
